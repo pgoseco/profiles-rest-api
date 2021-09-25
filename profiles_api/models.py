37db026473787f3263fe90eaf750c89dea084ad5
pgoseco <patrickgoseco@gmail.com>
2021-09-25 16:51:28 +1000

    Copy and pasted instructor's code

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,23 +5,23 @@ from django.db.models.expressions import Value
 
 # Create your models here.
 class UserProfileManager(BaseUserManager):
-    """Manger for user profiles ( UserProfile() )"""
-    
-    def CreateUser(self, email, name, password=None):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('Users must have an email address')
-        
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
 
-        user.set_password(password) #set_password automatically encripts this data
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
-    
-    def CreateSuperUser(self, email, name, password):
-        """Create and save new super user with given details"""
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
@@ -31,30 +31,26 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users in the system""" #docstring
-    
-    email = models.EmailField(max_length=255, unique=True) 
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True) #determines whether a users profile is activated or not, by default it is true.
-    is_staff = models.BooleanField(default=False) #determines whether a user is a staff, by default it is false.
-    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
-    def GetFullName(self):
-        """Retrieve Full Name of User"""
+    def get_full_name(self):
+        """Retrieve full name for user"""
         return self.name
 
-    def GetShortName(self):
-        """Retrieve Short Name of User"""
+    def get_short_name(self):
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """Return string representation of our User"""
+        """Return string representation of user"""
         return self.email
